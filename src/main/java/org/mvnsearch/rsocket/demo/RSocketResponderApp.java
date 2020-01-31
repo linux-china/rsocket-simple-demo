44e581b5812c3652a0d57f8de00de6d6887d582b
linux_china <libing.chen@gmail.com>
2020-01-31 09:53:55 -0800

    RSocket Responder demo app

---
+++ src/main/java/org/mvnsearch/rsocket/demo/RSocketResponderApp.java
@@ -0,0 +1,24 @@
+package org.mvnsearch.rsocket.demo;
+
+import io.rsocket.Closeable;
+import io.rsocket.RSocketFactory;
+import io.rsocket.uri.UriTransportRegistry;
+
+/**
+ * RSocket Responder demo app
+ *
+ * @author linux_china
+ */
+public class RSocketResponderApp {
+    public static void main(String[] args) throws Exception {
+        SimpleResponderFactory responderFactory = new SimpleResponderFactory();
+        String listenUri = "tcp://0.0.0.0:42252";
+        Closeable server = (Closeable) RSocketFactory.receive()
+                .acceptor(responderFactory.responder())
+                .transport(UriTransportRegistry.serverForUri(listenUri))
+                .start()
+                .block();
+        System.out.println("RSocket responder is listening on 42252!");
+        server.onClose().block();
+    }
+}
