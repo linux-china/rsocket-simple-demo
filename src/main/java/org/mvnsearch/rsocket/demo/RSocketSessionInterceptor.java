9b72569ff1aa97b47c5f29e731df7e20446e5498
linux_china <libing.chen@gmail.com>
2020-02-04 21:23:27 -0800

    RSocket session interceptor

---
+++ src/main/java/org/mvnsearch/rsocket/demo/RSocketSessionInterceptor.java
@@ -0,0 +1,68 @@
+package org.mvnsearch.rsocket.demo;
+
+import io.rsocket.AbstractRSocket;
+import io.rsocket.Payload;
+import io.rsocket.RSocket;
+import io.rsocket.plugins.RSocketInterceptor;
+import org.reactivestreams.Publisher;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+/**
+ * RSocket session interceptor
+ *
+ * @author linux_china
+ */
+public class RSocketSessionInterceptor implements RSocketInterceptor {
+    @Override
+    public RSocket apply(RSocket source) {
+        return new AbstractRSocket() {
+            private MutableContext mutableContext = new MutableContext();
+
+            @Override
+            public Mono<Void> fireAndForget(Payload payload) {
+                return source.fireAndForget(payload).subscriberContext(mutableContext::putAll);
+            }
+
+            @Override
+            public Mono<Payload> requestResponse(Payload payload) {
+                return source.requestResponse(payload).subscriberContext(mutableContext::putAll);
+            }
+
+            @Override
+            public Flux<Payload> requestStream(Payload payload) {
+                return source.requestStream(payload).subscriberContext(mutableContext::putAll);
+            }
+
+            @Override
+            public Flux<Payload> requestChannel(Publisher<Payload> payloads) {
+                return source.requestChannel(payloads).subscriberContext(mutableContext::putAll);
+            }
+
+            @Override
+            public Mono<Void> metadataPush(Payload payload) {
+                return source.metadataPush(payload).subscriberContext(mutableContext::putAll);
+            }
+
+            @Override
+            public void dispose() {
+                source.dispose();
+            }
+
+            @Override
+            public boolean isDisposed() {
+                return source.isDisposed();
+            }
+
+            @Override
+            public Mono<Void> onClose() {
+                return source.onClose();
+            }
+
+            @Override
+            public double availability() {
+                return source.availability();
+            }
+        };
+    }
+}
